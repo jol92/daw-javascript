4fb3cc1f3c4df803eb2ef9f03782514f1224cd65
jol92 <jorgeojedalopez92@gmail.com>
2018-09-30 11:48:03 +0200

    added commentary

--- UT2/ejercicio3/main.js
+++ UT2/ejercicio3/main.js
@@ -1,15 +1,22 @@
+/**
+ * Programa que lea un string introducido por el usuario.
+ * Comprueba que solo contenga letras.
+ * Cambia la primera letra a mayúscula y el resto a minúscula.
+ * Le quita los espacios a principio y final de la cadena.
+ */
+
 var text; // texto introducido por el usuario
 var regex = /^[a-zA-Z\s]*$/; // expresión regular que sólo admite letras (mayúsculas o minúsculas) y espacios.
 
 text = prompt("Introduzca el texto");
 
-while(!regex.test(text) || text == " " || text == ""){ // comprobación de string en bucle
+while (!regex.test(text) || text == " " || text == "") { // comprobación de string en bucle.
     text = prompt("Error. Introduzca el texto");
 }
 
-text = capitalize(text.trim());
+text = cleanUp(text.trim()); // se llama a la función cleanUp y se añade el método trim para borrar los espacios al principio y final de la cadena.
 document.write(text);
 
-function capitalize(string) {  // convierte la primera letra a mayúscula y la une al resto de la cadena
-    return string.charAt(0).toUpperCase() + string.slice(1);
+function cleanUp(string) {  // convierte la primera letra a mayúscula y la une al resto de la cadena convertida a minúscula.
+    return string.charAt(0).toUpperCase() + string.substr(1).toLowerCase();
 }
